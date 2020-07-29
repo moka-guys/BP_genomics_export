9d89d7291ee1db83b188f50203e44e06ff062fe5
Nana Mensah <Nana.mensah1@nhs.net>
2020-07-29 09:54:11 +0100

    Add powershell script to generate coversheets

---
+++ generate_coversheets.ps1
@@ -0,0 +1,1 @@
+S:\Genetics_Data2\Array\Software\Python-3.6.5\python.exe S:\Genetics\Bioinformatics\NGS\200511_BlueprintExport\src\blueprint_export_coversheet.py "S:\Genetics\Bioinformatics\NGS\200511_BlueprintExport\reports"
